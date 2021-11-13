839fe1c1758b526c318cad4e8816ea7002064e95
ne75 <ne75@cornell.edu>
2021-11-13 15:34:37 -0800

    add t32 test case

---
+++ test/src/t32.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int c;
+
+int main() {
+    printf("$\n"); // start of test character
+
+    int a = 1000;
+    int b = 2000;
+
+    printf("%d\n", a+b);
+
+    c = 4000;
+    printf("%d\n", c);
+
+    printf("~\n"); // end of test character
+    return 0;
+}
