08235bef72a94cf341b8167a7dc21ab13ae58aed
Nikita Ermoshkin <nikita@hitl1-1.local>
2022-02-09 10:42:20 -0800

    add more builtins

--- libc/stdlib/abs.c
+++ libc/stdlib/abs.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 
 long
-labs(long n)
-{
-  return (n < 0) ? -n : n;
+labs(long n) {
+  long x;
+  asm("abs %0, %1" : "=r"(x) : "r"(n));
+  return x;
 }
 
 #if defined(__GNUC__) && (_INT_SIZE == _LONG_SIZE)
