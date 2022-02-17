d7a2e4b1895569008da2dfb7dbbc8d2005e10702
Nikita Ermoshkin <nikita@hitl1-1.local>
2022-02-17 13:08:45 -0800

    add more builtins

--- test/src/t100.cpp
+++ test/src/t100.cpp
@@ -11,23 +11,23 @@
 #include <sys/p2es_clock.h>
 
 inline void _dirh(char pin) {
-    asm volatile  ("dirh %0\n"::"r"(pin));
+    asm volatile  ("dirh %0\n"::"r"((int)pin));
 }
 
 inline void _dirl(char pin) {
-    asm volatile  ("dirl %0\n"::"r"(pin));
+    asm volatile  ("dirl %0\n"::"r"((int)pin));
 }
 
 inline void _pinh(char pin) {
-    asm volatile  ("dirh %0\n"::"r"(pin));
+    asm volatile  ("dirh %0\n"::"r"((int)pin));
 }
 
 inline void _pinl(char pin) {
-    asm volatile  ("dirl %0\n"::"r"(pin));
+    asm volatile  ("dirl %0\n"::"r"((int)pin));
 }
 
 inline void _pinnot(char pin) {
-    asm volatile  ("outnot %0\n"::"r"(pin));
+    asm volatile  ("outnot %0\n"::"r"((int)pin));
 }
 
 inline int _pinr(char pin) {
@@ -58,22 +58,30 @@ void _waitus(unsigned s) {
    asm("waitx %0" :: "r"(s));
 }
 
-int get_p_test(int d, int c, int s) {
-   _dirl(d); // float answer
-   _waitus(s);
-   _pinh(c);
-   _waitus(s);
-   d = _pinr(d); //ACK=low
-   _pinl(c);
-   _waitus(s);
-   return d;
+int test(int x) {
+    int t;
+    char p1, p2, w;
+
+    t = x;
+    w = t >> 16;
+    p2 = (t >> 8) & 0xff;
+    p1 = t & 0xff;
+
+    _pinh(p1);
+    _pinh(p2);
+    _pinl(p2);
+    _pinl(p1);
+
+    return t;
 }
 
 int main() {
    _clkset(_SETFREQ, _CLOCKFREQ);
    _uart_init(DBG_UART_RX_PIN, DBG_UART_TX_PIN, 3000000);
 
-   get_p_test(1, 2, 100);
+   int i = 0x0a2829;
+
+   test(i);
    
    while(1) {
       waitcnt(CLKFREQ + CNT);
