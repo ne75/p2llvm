eddcc9232e60351ab2fd7086f13dd728c4ba3eba
Nikita Ermoshkin <nikita@hitl1-1.local>
2022-02-21 10:10:42 -0800

    clean up examples some more

--- examples/2_cogs/cogs.cpp
+++ examples/2_cogs/cogs.cpp
@@ -1,9 +1,4 @@
-/*
- * A basic test of starting cogs. This also uses the newer methods of pin control on P2
- * to avoid having to check if we should use register A or register B
- */
 #include <propeller.h>
-#include <sys/p2es_clock.h>
 
 struct led_mb_t {
     char pin;
@@ -21,13 +16,11 @@ void blink(void *par) {
 
     while(1) {
         outnot(led->pin);
-        waitcnt(led->delay + CNT);
+        waitx(led->delay);
     }
 }
 
 int main() {
-    _clkset(_SETFREQ, _CLOCKFREQ);
-
     led_mb_t led1 = {56, CLKFREQ};
     led_mb_t led2 = {57, CLKFREQ/2};
     led_mb_t led3 = {58, CLKFREQ/4};
@@ -36,5 +29,7 @@ int main() {
     cogstart(blink, (int)&led2, (int*)stack2, sizeof(stack2));
     cogstart(blink, (int)&led3, (int*)stack3, sizeof(stack3));
 
-    while(1);
+    while(1) {
+        waitx(CLKFREQ);
+    }
 }
