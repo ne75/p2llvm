235d89cfef50743503208bd4e523ffdc47b99077
ne75 <ne75@cornell.edu>
2022-03-25 10:12:43 -0700

    wip

--- examples/1_blink/blink.cpp
+++ examples/1_blink/blink.cpp
@@ -1,10 +1,25 @@
-#include <propeller.h>
+#define P2_TARGET_MHZ   300
+#include "propeller.h"
+#include "sys/p2es_clock.h"
 
 int main() {
-    dirh(56);
+    _clkset(_SETFREQ, _CLOCKFREQ);
+
+    drvl(59);
 
     while(1) {
-        outnot(56);
-        waitx(CLKFREQ);
+        drvl(56);
+        waitx(CLKFREQ/10000);
+        drvh(56);
+        waitx(24*(CLKFREQ/10000));
     }
+    // int f = 25;
+    // int t = _CLOCKFREQ/(2*f*6);
+    // while(1) {
+    //     for (int i = 56; i < 62; i++) {
+    //         drvl(i);
+    //         waitx(t);
+    //         drvh(i);
+    //     }
+    // }
 }
