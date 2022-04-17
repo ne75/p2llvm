3b15a214091da17e4b9171d5e7e42ae008ab2541
ne75 <ne75@cornell.edu>
2022-04-17 10:44:28 -0700

    restore blink example

--- examples/1_blink/blink.cpp
+++ examples/1_blink/blink.cpp
@@ -9,17 +9,6 @@ int main() {
 
     while(1) {
         drvl(56);
-        waitx(CLKFREQ/10000);
-        drvh(56);
-        waitx(24*(CLKFREQ/10000));
+        waitx(CLKFREQ);
     }
-    // int f = 25;
-    // int t = _CLOCKFREQ/(2*f*6);
-    // while(1) {
-    //     for (int i = 56; i < 62; i++) {
-    //         drvl(i);
-    //         waitx(t);
-    //         drvh(i);
-    //     }
-    // }
 }
