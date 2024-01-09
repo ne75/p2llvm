b927e50c13cd721e4a19d5b68e43db51eb5605a0
ne75 <ne75@cornell.edu>
2024-01-09 13:39:05 -0800

    test cleanup

--- test/src/t36.cpp
+++ test/src/t36.cpp
@@ -3,18 +3,11 @@
 #include <string.h>
 #include <math.h>
 
-#define P2_TARGET_MHZ   300
-#include "propeller.h"
-#include "sys/p2es_clock.h"
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 int main(void) {
-    _clkset(_SETFREQ, _CLOCKFREQ);
-    _uart_init(DBG_UART_RX_PIN, DBG_UART_TX_PIN, 2000000);
-
     printf("$\n");
 
 	printf("%s \n%c \n%d \n%lld \n%u \n%llu \n%f \n%.9f \n%x\n",
--- test/src/t37.cpp
+++ test/src/t37.cpp
@@ -3,10 +3,6 @@
 #include <string.h>
 #include <math.h>
 
-#define P2_TARGET_MHZ   300
-#include "propeller.h"
-#include "sys/p2es_clock.h"
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -25,9 +21,6 @@ float fs[] = {
 };
 
 int main(void) {
-    _clkset(_SETFREQ, _CLOCKFREQ);
-    _uart_init(DBG_UART_RX_PIN, DBG_UART_TX_PIN, 2000000);
-
     printf("$\n");
 	
 	for (int i = 0; i < sizeof(fs)/sizeof(float); i++) {
