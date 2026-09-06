3d5c5d18cd2fc50e26c7839306114e0ddd88b76a
nikita <nikita@airhartaero.com>
2026-09-05 21:50:12 -0700

    cleanup: retire basic arithmetic and data-layout demos

--- test/src/t0.cpp
+++
@@ -1,7 +0,0 @@
-#include <propeller.h>
-
-int main() {
-    volatile int x = 0;
-    x = 10000;
-    return 0;
-}
--- test/src/t1.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("$\n"); // start of test character
-    printf("Hello\nWorld!\n");
-    printf("~\n"); // end of test character
-
-    return 0;
-}
